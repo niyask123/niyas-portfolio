75df1b3aac1b234d4cb0ad11542fc5977f1c0b23
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-07-30 12:02:30 +0530

    uploaded

--- src/components/pageComponents/Header.jsx
+++ src/components/pageComponents/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -64,6 +65,9 @@ function Header() {
             </li>
             <li>
               <a>CONTACT</a>
+            </li>
+            <li>
+                <Link to={"/uploadPage"}>Login</Link>
             </li>
           </ul>
         </div>
