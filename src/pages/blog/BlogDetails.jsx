eeed774b42622c3b0ecdd6aeff80e2fbde234741
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-08-01 16:47:37 +0530

    udpated

---
+++ src/pages/blog/BlogDetails.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const BlogDetails = () => {
+  return (
+    <div className="py-12 px-36">
+      <div className="flex  gap-5 justify-between">
+        <div className="flex w-full gap-7">
+          <img src="/public/image/personal/niyas1.png" alt="" />
+          <div className="flex flex-col">
+            <h2>Heading</h2>
+            <p>Titile</p>
+            <p>1</p>
+            <p>1</p>
+          </div>
+        </div>
+        <div className="flex w-full gap-7">
+          <img src="/public/image/personal/niyas1.png" alt="" />
+          <div className="flex flex-col">
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetails;
