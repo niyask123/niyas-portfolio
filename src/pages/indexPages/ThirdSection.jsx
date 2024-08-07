3b99b2ca760f088fef468e3d36c6daa661bbef27
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-08-07 09:46:10 +0530

    udpated

--- src/pages/indexPages/ThirdSection.jsx
+++ src/pages/indexPages/ThirdSection.jsx
@@ -5,10 +5,10 @@ const ThirdSection = () => {
     <>
       <div className=" lg:px-20 px-3 flex-row gap-12 py-12 text-left justify-between ">
         <div className="text-center flex-col gap-3 items-center flex justify-center">
-          <span className="py-1 font-semibold text-3xl"> Skills</span>
+          <span className="py-1 font-semibold text-3xl"> Tech Stacks</span>
           <p>The skills, tools and technologies I am really good at:</p>
         </div>
-        <div className="grid grid-cols-2 lg:grid-cols-8 gap-3 pt-16">
+        <div className="grid grid-cols-2 lg:grid-cols-8 gap-3 gap-y-10 pt-16">
           <div className="flex flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/react.svg"
@@ -65,6 +65,48 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">TailwindCSS</p>
           </div>
+          <div className="flex flex-col gap-3 text-center">
+            <img
+              src="/image/logo/skills/bootstrap.png"
+              className="object-contain h-14"
+              alt=""
+            />
+            <p className="font-semibold">Bootstrap</p>
+          </div>
+
+          <div className="flex flex-col gap-3 text-center">
+            <img
+              src="/image/logo/skills/php.png"
+              className="object-contain h-14"
+              alt=""
+            />
+            <p className="font-semibold">PHP</p>
+          </div>
+          <div className="flex flex-col gap-3 text-center">
+            <img
+              src="/image/logo/skills/html.png"
+              className="object-contain h-14"
+              alt=""
+            />
+            <p className="font-semibold">HTML</p>
+          </div>
+          <div className="flex flex-col gap-3 text-center">
+            <img
+              src="/image/logo/skills/css-3.png"
+              className="object-contain h-14"
+              alt=""
+            />
+            <p className="font-semibold">CSS3</p>
+          </div>
+          <div className="flex flex-col gap-3 text-center">
+            <img
+              src="/image/logo/skills/jQuery.png"
+              className="object-contain h-14"
+              alt=""
+            />
+            <p className="font-semibold">jQuery</p>
+          </div>
+          
           <div className="flex flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/git.svg"
