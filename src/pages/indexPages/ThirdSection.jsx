00c4183980074575b85a4ae02aa2ba9470ece3d4
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-08-07 14:56:06 +0530

    udpated

--- src/pages/indexPages/ThirdSection.jsx
+++ src/pages/indexPages/ThirdSection.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const ThirdSection = () => {
   return (
     <>
-      <div className=" lg:px-20 px-3 flex-row gap-12 py-12 text-left justify-between ">
+      <div className="group lg:px-20 px-3 flex-row gap-12 py-12 text-left justify-between ">
         <div className="text-center flex-col gap-3 items-center flex justify-center">
           <span className="py-1 font-semibold text-3xl"> Tech Stacks</span>
           <p>The skills, tools and technologies I am really good at:</p>
         </div>
         <div className="grid grid-cols-2 lg:grid-cols-8 gap-3 gap-y-10 pt-16">
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/react.svg"
               className="object-contain h-14"
@@ -17,7 +17,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">React JS</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/js.svg"
               className="object-contain h-14"
@@ -25,7 +25,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">JavaScript</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/ts.svg"
               className="object-contain h-14"
@@ -33,7 +33,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">TypeScript</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/nodejs.svg"
               className="object-contain h-14"
@@ -41,7 +41,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">nodeJS</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/postgresql.svg"
               className="object-contain h-14"
@@ -49,7 +49,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">Postgresql</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/sass.svg"
               className="object-contain h-14"
@@ -57,7 +57,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">SASS</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/tailwindcss.svg"
               className="object-contain h-14"
@@ -65,7 +65,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">TailwindCSS</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/bootstrap.png"
               className="object-contain h-14"
@@ -74,7 +74,7 @@ const ThirdSection = () => {
             <p className="font-semibold">Bootstrap</p>
           </div>
 
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/php.png"
               className="object-contain h-14"
@@ -82,7 +82,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">PHP</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/html.png"
               className="object-contain h-14"
@@ -90,7 +90,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">HTML</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/css-3.png"
               className="object-contain h-14"
@@ -98,7 +98,7 @@ const ThirdSection = () => {
             />
             <p className="font-semibold">CSS3</p>
           </div>
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/jQuery.png"
               className="object-contain h-14"
@@ -107,7 +107,7 @@ const ThirdSection = () => {
             <p className="font-semibold">jQuery</p>
           </div>
           
-          <div className="flex flex-col gap-3 text-center">
+          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/git.svg"
               className="object-contain h-14"
