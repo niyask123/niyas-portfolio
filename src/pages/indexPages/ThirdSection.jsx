28588e96c9c38ce41473178bf4b1ecc1423e8cf2
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-08-07 15:11:35 +0530

    udpated

--- src/pages/indexPages/ThirdSection.jsx
+++ src/pages/indexPages/ThirdSection.jsx
@@ -8,112 +8,112 @@ const ThirdSection = () => {
           <span className="py-1 font-semibold text-3xl"> Tech Stacks</span>
           <p>The skills, tools and technologies I am really good at:</p>
         </div>
-        <div className="grid grid-cols-2 lg:grid-cols-8 gap-3 gap-y-10 pt-16">
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500 flex-col gap-3 text-center">
+        <div className="grid grid-cols-2 lg:grid-cols-7 gap-3 gap-y-10 pt-16">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/react.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">React JS</p>
+            <p className="font-semibold  p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500 ">React JS</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/js.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">JavaScript</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">JavaScript</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/ts.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">TypeScript</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">TypeScript</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/nodejs.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">nodeJS</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">nodeJS</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/postgresql.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">Postgresql</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">Postgresql</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/sass.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">SASS</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">SASS</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/tailwindcss.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">TailwindCSS</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">TailwindCSS</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/bootstrap.png"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">Bootstrap</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">Bootstrap</p>
           </div>
 
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/php.png"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">PHP</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">PHP</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/html.png"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">HTML</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">HTML</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/css-3.png"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">CSS3</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">CSS3</p>
           </div>
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/jQuery.png"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">jQuery</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">jQuery</p>
           </div>
           
-          <div className="flex p-3 group-hover:bg-yellow-500 group-hover:rounded-2xl duration-500  flex-col gap-3 text-center">
+          <div className="flex p-3 flex-col gap-3 text-center">
             <img
               src="/image/logo/skills/git.svg"
               className="object-contain h-14"
               alt=""
             />
-            <p className="font-semibold">Git</p>
+            <p className="font-semibold p-1 group-hover:bg-yellow-500 group-hover:rounded-md duration-500">Git</p>
           </div>
         </div>
       </div>
